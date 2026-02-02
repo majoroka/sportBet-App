242bb9f298561793f5e9ad498a17e59016b20084
majoroka <majoroka@gmail.com>
2026-02-02 19:53:39 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Define a base path para o GitHub Pages
+  // Se a variável de ambiente VITE_BASE_PATH existir (definida no workflow), usa-a.
+  // Caso contrário, usa '/' (para desenvolvimento local).
   base: process.env.VITE_BASE_PATH || '/',
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './tests/setup.ts', // Garante que o ficheiro está em /tests/setup.ts
-  },
 });
